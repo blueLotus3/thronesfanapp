f4348cf3bb329e70b1ed860b1bde3c19ddbfb3f8
Troy Lemons Jr <troylemonsjr@gmail.com>
2024-09-07 15:09:26 -0700

    added Roster page as template

---
+++ src/components/Roster.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+
+const Roster = () => {
+    return(
+        <div className="rosterDiv">
+            <h1>This is where all characters will show up</h1>
+        </div>
+    )
+} 
+
+
+export default Roster;
